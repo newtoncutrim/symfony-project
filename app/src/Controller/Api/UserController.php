5c0cd8f4f7397ca5639858fb658fde59a4fe1d4c
newtoncutrim <newtoncutrim777@gmail.com>
2024-10-25 15:51:43 -0300

    :lock: feat: adicionado login de user, email e password

---
+++ app/src/Controller/Api/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/api/user', name: 'app_api_user')]
+    public function index(): Response
+    {
+        return $this->render('api/user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
